de74e69237c33e9ce4679861da0ec23c515bd8e2
daniemart <github email address>
2019-10-13 16:37:24 -0700

    added remaining styles and pacman with credit

--- danie-website/src/components/Blogs.js
+++ danie-website/src/components/Blogs.js
@@ -6,10 +6,12 @@ class Blogs extends Component {
     render() {
         const url = 'https://medium.com/@daniemart5'
         return (
-
+        <div className="blogbox">
+            <h1>BLOGS</h1>
             <div className='blogbox'>
                 <iframe width="950" height="550" className="blogs" src={url}/> 
             </div>
+        </div>
         )
     }
 }
