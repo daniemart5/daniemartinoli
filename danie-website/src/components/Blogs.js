b7795601f43f6d574c57824cb68d9e12f77690e3
daniemart <github email address>
2019-10-13 15:12:17 -0700

    adjusted blog style

--- danie-website/src/components/Blogs.js
+++ danie-website/src/components/Blogs.js
@@ -6,12 +6,9 @@ class Blogs extends Component {
     render() {
         const url = 'https://medium.com/@daniemart5'
         return (
-            <div>
 
-            <h1>Check these out!</h1>
-
-            <iframe width="950" height="550" className="blogs" src={url}/> 
-
+            <div className='blogbox'>
+                <iframe width="950" height="550" className="blogs" src={url}/> 
             </div>
         )
     }
