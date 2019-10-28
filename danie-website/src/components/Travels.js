ac1529defa18d3ef7515bf373f96cee3f400ff88
daniemart <github email address>
2019-10-27 18:38:21 -0700

    added my travels page!

---
+++ danie-website/src/components/Travels.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Travels extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My Travels 🌎</h1>
+                <div></div>
+            </div>
+        )
+    }
+}
+export default Travels
