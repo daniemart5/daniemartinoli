60f7220ded8617d2e085d5788f0498666c81f135
daniemart <github email address>
2019-10-27 18:46:24 -0700

    added content to travels page

--- danie-website/src/components/Travels.js
+++ danie-website/src/components/Travels.js
@@ -5,7 +5,19 @@ class Travels extends Component {
         return (
             <div>
                 <h1>My Travels 🌎</h1>
-                <div></div>
+                <p>Outside of my professional life I enjoy traveling the world with my hubby nick!</p>
+                <p>Here are some of my favorite pictures:</p>
+
+                <h3>Show gallery of pictures here</h3>
+                <h3>Places I have been</h3>
+                <ul>
+                    <li>Brussels, Belguim</li>
+                    <li>Ghent and Bruges, Belguim</li>
+                    <li>London, England</li>
+                    <li>Paris, France</li>
+                    <li>Amsterdam, Neitherlands</li>
+                    <li>Cancun, Mexico</li>
+                </ul>
             </div>
         )
     }
